64775ceb737a0482cc1dac44ca08b9ac1592a174
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-10-22 17:19:05 +0530

    feat: Api added for cloud vision

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const vision = require('@google-cloud/vision');
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    res.status(200).json({ "success": "success" })
+router.post("/", async (req, res) => {
+    const { route } = req.body;
+    try {
+        const client = new vision.ImageAnnotatorClient({
+            keyFilename: "./APIKey.json"
+        })
+        const [result] = await client.textDetection(route);
+        const annonations = result.textAnnotations;
+        res.status(200).json({ "code": annonations[0].description })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ "error": "Server Error!" })
+    }
 })
 
 module.exports = router;
